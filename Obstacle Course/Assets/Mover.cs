b4c4c4dfd535344bf27fe8c3d7d0d19aa4fc7538
Tijil Agrawal <tijil2008@gmail.com>
2021-08-11 08:59:46 +0530

    Added Variables

--- Obstacle Course/Assets/Mover.cs
+++ Obstacle Course/Assets/Mover.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+    float xValue = 0f;
+    float yValue = 0.01f;
+    float zValue = 0f;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +21,7 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        transform.Translate(xValue, yValue, zValue);
 
     }
 }
